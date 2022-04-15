807d1912ca4d361bd91739809130d89878ad92ec
harish kumar <harishpathania150@gmail.com>
2022-04-15 21:37:08 +0530

    update the program

--- multithreading-fcc.py
+++ multithreading-fcc.py
@@ -28,51 +28,53 @@
         
 #     print('end main')
 
-from threading import Thread, Lock
-import time
+# from threading import Thread, Lock
+# import time
 
 
-database_value = 0
+# database_value = 0
 
-def increase(lock):
-    global database_value
-    # lock is use for having a better result in thread ( when you start the lock you have to end it also otherwise this will gave error to you)
-    # if i do not want to gave a start or release function to the lock we can gave a lock function to our function were we use the lock
-    with lock: # in this we can also create the lock to the thread 
+# def increase(lock):
+#     global database_value
+#     # lock is use for having a better result in thread ( when you start the lock you have to end it also otherwise this will gave error to you)
+#     # if i do not want to gave a start or release function to the lock we can gave a lock function to our function were we use the lock
+#     with lock: # in this we can also create the lock to the thread 
         
-    # lock.acquire()
-        local_copy = database_value
-     
-    # processing
-        local_copy += 1
-        time.sleep(0.1)
-        database_value = local_copy
-        # lock.release()
+#         local_copy = database_value
+#     # lock.acquire()
+#     # processing
+#         local_copy += 1
+#         time.sleep(0.1)
+#         database_value = local_copy
+#         # lock.release()
     
 
-if __name__ =="__main__":
+# if __name__ =="__main__":
     
-    lock = Lock()
-    
-    print('start value',database_value)
-    
-    
-    thread1 = Thread(target=increase,args=(lock,))
-    thread2 = Thread(target=increase,args=(lock,))
-    
-    thread1.start()
-    thread2.start()
-    
-    thread1.join()
-    thread2.join()
-    
-    print('end value',database_value)
-    
-    print('end main')
+     
+#     lock = Lock()
+
+#     print('start value',database_value)
     
     
     
+#     thread1 = Thread(target=increase,args=(lock,))
+#     thread2 = Thread(target=increase,args=(lock,))
+
+#     thread1.start()
+#     thread2.start()
+    
+#     thread1.join()
+#     thread2.join()
+    
+#     print('end value',database_value)
     
     
+#     print('end main')
     
+    
+# queue =  Like stack, queue is a linear data structure that stores items in First In First Out (FIFO) manner. With a queue the least recently added item is removed first. A good example of queue is any queue of consumers for a resource where the consumer that came first is served first.
+
+
+
     
