d3b1f2e6d79d8c98db3ea49e2934baf86da18f68
harish kumar <harishpathania150@gmail.com>
2022-04-15 21:56:14 +0530

    update the program

--- multithreading-fcc.py
+++ multithreading-fcc.py
@@ -76,5 +76,34 @@
 # queue =  Like stack, queue is a linear data structure that stores items in First In First Out (FIFO) manner. With a queue the least recently added item is removed first. A good example of queue is any queue of consumers for a resource where the consumer that came first is served first.
 
 
+# Enqueue: Adds an item to the queue. If the queue is full, then it is said to be an Overflow condition – Time Complexity : O(1)
+# Dequeue: Removes an item from the queue. The items are popped in the same order in which they are pushed. If the queue is empty, then it is said to be an Underflow condition – Time Complexity : O(1)
+# Front: Get the front item from queue – Time Complexity : O(1)
+# Rear: Get the last item from queue – Time Complexity : O(1)
+
+# example of queue:- how queue work
+from threading import Thread
+from queue import Queue
+import time
+
+if __name__ == "__main__":
+    q = Queue()
+    
+    
+    q.put(1)
+    q.put(2)
+    q.put(3)
+    
+    # 3 2 1 -->
+    first = q.get()
+    print(first)
+    
+    
+    # q.task_done()
+    # q.join()
+     
+    print('end main')
+    
+
 
     
