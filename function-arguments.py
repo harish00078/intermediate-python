2074e7be43113af7d6b993066a85015aaba343d6
harish kumar <harishpathania150@gmail.com>
2022-04-22 23:02:31 +0530

    upgrade

--- function-arguments.py
+++ function-arguments.py
@@ -35,20 +35,95 @@
 
 
 # that method is called variable length argument.
-def foo(a,b,*args,**kwargs): # we do not change the position or keyword of the values directly thats why in function  we use the argument for want to put a additional value or keyword-argument is use if we want to add a keyword value
-    print(a,b)
-    for arg in args:
-        print(arg)
-    for key in kwargs:
-        print(key, kwargs[key])
+# def foo(a,b,*args,**kwargs): # we do not change the position or keyword of the values directly thats why in function  we use the argument for want to put a additional value or keyword-argument is use if we want to add a keyword value
+#     print(a,b)
+#     for arg in args:
+#         print(arg)
+#     for key in kwargs:
+#         print(key, kwargs[key])
         
         
         
-foo(1,2,six=6,seven=7)
-foo(1,2,3,4,5)
-foo(1,2)
+# foo(1,2,six=6,seven=7)
+# foo(1,2,3,4,5)
+# foo(1,2)
 
 
+# force (arguments work with keyword arguments).
+# def foo(a, b, *, c, d):
+#     print(a,b,c,d)
+    
+# def foo(*args, last):
+#     for arg in args:
+#         print(arg)
+#     print(last)
+    
+    
+    
+# foo(1,2,3, last=100)
+
+
+
+# use list ,tuples, dictionary in function arguments.
+# when we using  list,tuples,dictionary in function arguments we have to put a value acc to our variables.
+# def foo(a,b,c):
+#     print(a,b,c)
 
     
+
+
+# my_dict = {'a':1,'b':2,'c':3}
+# my_dict = {'e':1,'b':2,'c':3}  # this dictionary will gave me the error. 
+   
+# my_list = [0,1,2]
+# my_list = [0,1,2,3] # this will not work beacuse it represent extra variable
+
+# foo(*my_list)
+    
+    
+
+
+
+
+
+# local and global variables.
+
+# def foo():
+#     global number  # this (global number) function will convert gobal variable into local variable
+#     x = number# (x) is my local variable
+#     number = 3 # global variable function will convert this local variable into global variable
+#     print('number inside function',x)
+    
+
+
+
+# number = 0 # (number)is our global variable
+# foo()  
+# print(number)
+
+# paramater passing.(by value or by reference).
+
+def foo(a_list):
+    # x = 5
+    a_list += [200,300,400]
+    # a_list.append(4)
+    # a_list[0] = -100
+    
+    
+
+    
+my_list = [1,2,3]    
+# var = 10
+foo(my_list)
+print(my_list)
+    
+
+
+
+
+
+
+
+
+
 
